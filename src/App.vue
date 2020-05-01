d895bc260d26acb63d2948881dc1ef22b7ae1ba4
Rita Bradley <contact@ritabradley.com>
2020-05-01 14:15:41 -0700

    more configuration
    - setup fonts

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="text-2xl font-bold">Hello World</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+    };
+</script>
+
+<style></style>
